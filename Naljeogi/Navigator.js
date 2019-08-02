8a1d3c0a152b014e05d3dc6452bfc44eea845034
like-hanbin <likehanbin@gmail.com>
2019-08-02 11:42:38 +0900

    Update navigator

--- Naljeogi/Navigator.js
+++ Naljeogi/Navigator.js
@@ -1,17 +1,57 @@
 import React from 'react';
-import {createAppContainer, createBottomTabNavigator } from "react-navigation"
-import {Platform} from "react-native"
+import {createAppContainer,
+        createBottomTabNavigator,
+        createStackNavigator } from "react-navigation"
+import {View} from "react-native"
 import DiaryList from "./screens/DiaryList"
 import MyPage from "./screens/MyPage"
-import Ionicons from '@expo/vector-icons'
+import AddPost from "./screens/AddPost"
+import {Ionicons} from '@expo/vector-icons'
 
 const MainNavigation = createBottomTabNavigator({
     DiaryList: {
         screen: DiaryList,
+        navigationOptions: {
+           tabBarIcon: () => {
+               return <Ionicons name='ios-apps' size={25}/>
+           }
+        }
+    },
+    AddPost: {
+        screen: () => null,
+        navigationOptions: {
+            tabBarIcon: () => {
+                return <Ionicons name='ios-create' size={25} color="tomato"/>
+            },
+            tabBarOnPress: ({navigation}) => {
+                navigation.navigate('Edit');
+            }
+        }
     },
     MyPage: {
         screen: MyPage,
+        navigationOptions: {
+            tabBarIcon: () => {
+                return <Ionicons name='md-person'size={25}/>
+            }
+         }
+    }
+},
+{
+    tabBarOptions: {
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+        showLabel: false
     }
 });
 
-export default createAppContainer(MainNavigation)
+const AppNavigator = createStackNavigator ({
+    Edit: AddPost,
+    Tab: MainNavigation,
+},{
+    initialRouteName: 'Tab',
+    mode : 'modal',
+    headerMode: 'none'
+});
+
+export default createAppContainer(AppNavigator)
--- Naljeogi/screens/AddPost.js
+++ Naljeogi/screens/AddPost.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Text } from 'react-native';
 import {SafeAreaView} from 'react-navigation';
-import CalendarPicker from 'react-native-calendar-picker';
 
-export default DiaryList = () => {
+export default AddPost = () => {
     return (
         <SafeAreaView>
-            <CalendarPicker/>
+            <Text>AddPost</Text>
         </SafeAreaView>
     );
 }
