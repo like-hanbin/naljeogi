226f1caa66d78848c63503c7fdb383a0bdf2deac
like-hanbin <likehanbin@gmail.com>
2019-08-04 22:42:16 +0900

    update screens

---
+++ Naljeogi/screens/ListScreen.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Text,View,FlatList,StyleSheet,Dimensions,ScrollView,TouchableOpacity } from 'react-native';
+import {SafeAreaView} from 'react-navigation';
+import CalendarPicker from 'react-native-calendar-picker';
+
+const {width,height} = Dimensions.get('window');
+
+export default ListScreen = (props) => {
+    return (
+        <SafeAreaView>
+            <View style={styles.diaryContainer}>
+                <CalendarPicker
+                    selectedStartDate={props.screenProps.selectedDate}
+                    previousTitle="<"
+                    nextTitle=">"
+                    todayTextStyle={{fontWeight: 'bold',textDecorationLine: 'underline'}}
+                    todayBackgroundColor={'transparent'}
+                    selectedDayColor="#ffe28c"
+                    onDateChange={props.screenProps.changeDate}/>
+                <ScrollView style={styles.listContainer}>
+                    <FlatList
+                        data={props.screenProps.Posts.filter(data => { return data.date == props.screenProps.selectedDate})}
+                        renderItem={({item,index}) => {
+                            return (
+                                <TouchableOpacity
+                                    activeOpacity={0.8}
+                                    onPress={() => {props.navigation.navigate('View',{id:item.id})}}>
+                                    <View style={styles.itemContainer}>
+                                        <Text style={styles.title} numberOfLines={1}>{item.title}</Text>
+                                        <Text style={styles.content} numberOfLines={1}>{item.content}</Text>
+                                    </View>
+                                </TouchableOpacity>
+                            );}}
+                        keyExtractor={(_, index) => { return `${index}`}}/>
+                </ScrollView>
+            </View>
+            
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+
+    listContainer: {
+        marginLeft:25,
+    },
+    title:{
+        fontSize: 17,
+        color: "#3b3b3b",
+        fontWeight: "600",
+    },
+    content:{
+        fontSize: 13,
+        paddingTop: 5,
+        color: "gray"
+    },
+    date:{
+        fontSize: 10,
+        color: "gray"
+    },
+    itemContainer: {
+        flex: 1,
+        width: width-60,
+        paddingTop:10,
+        paddingLeft:10,
+        marginBottom:20,
+        borderColor: "#7a7171",
+        borderTopWidth: 1,
+        borderLeftWidth: 1,
+        borderRightWidth: 7,
+    }
+})
