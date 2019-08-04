a182414935bb941ea6496a3eee4d2fbbfa6c1015
like-hanbin <likehanbin@gmail.com>
2019-08-04 22:45:44 +0900

    add viewheader

--- Naljeogi/components/EditHeader.js
+++ Naljeogi/components/EditHeader.js
@@ -3,7 +3,7 @@ import {View,TextInput,Text,TouchableOpacity,StyleSheet} from 'react-native'
 import {withNavigation} from 'react-navigation'
 import { Ionicons } from '@expo/vector-icons';
 
-const EditHeader = ({navigation,addPost}) => {
+const EditHeader = ({navigation,addPost,selectPicture}) => {
     return (
         <View style={styles.container}>
             <TouchableOpacity
@@ -12,15 +12,23 @@ const EditHeader = ({navigation,addPost}) => {
                 hitSlop={{top:32, bottom:32, left:32, right: 32}}>
                 <Ionicons name="ios-arrow-back" size={25} color={'#7a7171'}></Ionicons>
             </TouchableOpacity>
-            <TouchableOpacity
-                activeOpacity={0.8}
-                onPress= {() => {
-                    addPost();
-                    navigation.navigate('DiaryList');
-                }}
-                hitSlop={{top:32, bottom:32, left:32, right: 32}}>
-                <Ionicons name="ios-save" size={25} color={'#7a7171'}></Ionicons>
-            </TouchableOpacity>
+            <View style={styles.iconContainer}>
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    hitSlop={{top:2, bottom:2, left:2, right: 2}}
+                    onPress= {selectPicture}>
+                    <Ionicons name="ios-image" size={25} color={'#7a7171'}></Ionicons>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    hitSlop={{top:2, bottom:2, left:2, right: 2}}
+                    onPress= {() => {
+                        addPost();
+                        navigation.navigate('ListScreen');
+                    }}>
+                    <Ionicons name="ios-save" size={25} color={'#7a7171'}></Ionicons>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -29,6 +37,11 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    iconContainer: {
+        flexDirection: 'row',
+        width: 60,
         justifyContent: 'space-between'
     }
 })
---
+++ Naljeogi/components/ViewHeader.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View,TouchableOpacity,StyleSheet} from 'react-native'
+import {withNavigation} from 'react-navigation'
+import { Ionicons } from '@expo/vector-icons';
+
+const ViewHeader = ({navigation,addPost}) => {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={() => {navigation.goBack()}}
+                hitSlop={{top:32, bottom:32, left:32, right: 32}}>
+                <Ionicons name="ios-arrow-back" size={25} color={'#7a7171'}></Ionicons>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    }
+})
+
+export default withNavigation(ViewHeader);
